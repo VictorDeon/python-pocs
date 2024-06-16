16fd6a2f0ee09fdb900b3da4489ae9901e623f22
Victor Deon <victorhad@gmail.com>
2024-06-16 16:32:05 -0300

    input validation is inside controllers

--- domains/user_cases/user_retrieve.py
+++ domains/user_cases/user_retrieve.py
@@ -14,20 +14,10 @@ class UserRetrieve(UserRetrieveInterface):
 
         self.__users_repository = users_repository
 
-    def __validate_params(self, _id: int) -> None:
-        """
-        Valida os parâmetros de entrada.
-        """
-
-        if not _id:
-            raise ValueError("Identificador inválido para a busca.")
-
     def find(self, _id: int) -> dict:
         """
         Encontra o usuário pelo email.
         """
-
-        self.__validate_params(_id)
 
         user = self.__users_repository.retrieve(_id)
 
