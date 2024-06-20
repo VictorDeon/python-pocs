559109cf1cd264df364fd18f850d46035d081ce4
Victor Hugo Arnaud Deon <victor.arnaud@cerc.inf.br>
2024-06-20 10:36:04 -0300

    Insert domains.entity into presenters

--- src/adapters/presenters/find_user.py
+++ src/adapters/presenters/find_user.py
@@ -1,6 +1,7 @@
 from src.adapters.dtos import FindUserOutputDTO
 from src.adapters.interfaces import PresenterInterface
-from src.domains.entities import User
+from src.infrastructure.databases.models import User as UserModel
+from src.domains.entities import User, Profile, Permission, Group
 
 
 class FindUserPresenter(PresenterInterface):
@@ -8,9 +9,38 @@ class FindUserPresenter(PresenterInterface):
     Formatação de saída da API que busca um usuário.
     """
 
-    def present(self, user: User) -> FindUserOutputDTO:
+    def present(self, user: UserModel) -> FindUserOutputDTO:
         """
         Forma final de apresentação dos dados.
         """
 
-        return FindUserOutputDTO(user=user)
+        entity = User(
+            id=user.id,
+            name=user.name,
+            email=user.email,
+            profile=Profile(
+                id=user.profile.id,
+                phone=user.profile.phone,
+                address=user.profile.address
+            ),
+            permissions=[
+                Permission(
+                    id=permission.id,
+                    name=permission.name
+                ) for permission in user.permissions
+            ],
+            groups=[
+                Group(
+                    id=group.id,
+                    name=group.name,
+                    permissions=[
+                        Permission(
+                            id=permission.id,
+                            name=permission.name
+                        ) for permission in group.permissions
+                    ]
+                ) for group in user.groups
+            ]
+        )
+
+        return FindUserOutputDTO(user=entity)
