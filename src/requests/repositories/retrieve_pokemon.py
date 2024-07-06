fc6d5c3d86a99d326fdf45c58ab72433080f5c51
Victor Deon <victorhad@gmail.com>
2024-07-06 17:08:34 -0300

    Fix retrieve pokemon

--- src/requests/repositories/retrieve_pokemon.py
+++ src/requests/repositories/retrieve_pokemon.py
@@ -2,7 +2,7 @@ from ..presenters import RetrievePokemonPresenter
 from ..dtos import RetrievePokemonInputDTO
 from src.engines.caches import RedisCache
 from src.engines.clients import HTTPxClient
-from src.engines.requests import PokemonPokeAPIRepository
+from src.engines.requests import PokemonQueueRequestRepository
 
 
 class RetrievePokemonRepository:
@@ -23,7 +23,7 @@ class RetrievePokemonRepository:
         """
 
         async with HTTPxClient() as client, RedisCache() as cache:
-            repository = PokemonPokeAPIRepository(client, cache)
+            repository = PokemonQueueRequestRepository(client, cache)
             output = RetrievePokemonPresenter()
             model = await repository.get_by_id(self.input.id)
             return await output.present(model)
